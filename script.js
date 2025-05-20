543df2b6863f6bdba4f3da8e1a66b8e66bfd271c
xrob101 <robertssekyene05@gmail.com>
2025-05-20 15:25:51 +0300

    feat: add a colorPicker

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const grid = document.querySelector('#grid');
-
 /*
-  creates a nXn grid
+creates a nXn grid
 */
 function createGrid(numOfSquares) {
   for (let i = 0; i < numOfSquares; i++) {
@@ -16,28 +14,43 @@ function createGrid(numOfSquares) {
   }
 }
 
-createGrid(96);
+/** GLOBAL SCOPE **/
+const grid = document.querySelector('#grid');
+const colorPicker = document.querySelector('#colorPicker');
 
-const rows = Array.from(grid.children);
+createGrid(90);
+createGridShadingEffect(colorPicker.value);
 
-// add a shading effect to all pixels
-for (const row of rows) {
-  cols = Array.from(row.children);
-  cols.forEach(col => {
-    col.addEventListener('mousedown', e => {
-      // prevent the left button from dragging
-      if (e.button === 0) // 0 represents the left button
-        e.preventDefault();
-      col.style.backgroundColor = '#050505';
-    });    
-    col.addEventListener('mouseenter', e => {
-      // shade the pixel if the left button is held down while hovering over it
-      if (e.buttons === 1) { // 1 represents the left button when using the `buttons` (not `button`) property
-        col.style.backgroundColor = '#050505';
-      }
+/** END GLOBAL SCOPE **/
+
+// creates a shading effect to all pixels in the grid
+function createGridShadingEffect(pixelColor) {
+  const rows = Array.from(grid.children);
+  for (const row of rows) {
+    cols = Array.from(row.children);
+    cols.forEach(col => {
+      col.addEventListener('mousedown', e => {
+        // prevent the left button from dragging
+        if (e.button === 0) // 0 represents the left button
+          e.preventDefault();
+        setPixelFillColor(col, pixelColor);
+      });    
+      col.addEventListener('mouseenter', e => {
+        // shade the pixel if the left button is held down while hovering over it
+        if (e.buttons === 1) { // 1 represents the left button when using the `buttons` (not `button`) property
+          setPixelFillColor(col, pixelColor);
+        }
+      });
+    
     });
-  
-  });
+  }
+
 }
 
+colorPicker.addEventListener('input', e => {
+  createGridShadingEffect(colorPicker.value);
+});
 
+function setPixelFillColor (pixelElem, fillColor) {
+  pixelElem.style.backgroundColor = fillColor;
+}
